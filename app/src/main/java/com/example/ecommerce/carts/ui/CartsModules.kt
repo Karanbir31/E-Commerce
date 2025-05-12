942e42a11d3ec34e05208fbd9c5ed19465b11e73
KARANBIR SINGH <karan0031singh@gmail.com>
2025-05-12 12:07:20 +0530

    add carts section hilt modules

---
+++ app/src/main/java/com/example/ecommerce/carts/ui/CartsModules.kt
@@ -0,0 +1,45 @@
+package com.example.ecommerce.carts.ui
+
+import com.example.ecommerce.carts.data.CartsApi
+import com.example.ecommerce.carts.data.CartsRepositoryImp
+import com.example.ecommerce.carts.domain.CartsRepository
+import com.example.ecommerce.carts.domain.usecases.GetUsersCartUseCase
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class CartsRepositoryBinder {
+    @Binds
+    abstract fun repositoryBinder(cartsRepositoryImp: CartsRepositoryImp): CartsRepository
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CartsModules {
+
+    @Provides
+    @Singleton
+    fun providerCartsApi(
+        retrofit: Retrofit
+    ): CartsApi {
+        return retrofit.create(CartsApi::class.java)
+    }
+
+
+    @Provides
+    @Singleton
+    fun providerGetUsersCartUseCase(
+        repository: CartsRepository
+    ): GetUsersCartUseCase {
+        return GetUsersCartUseCase(repository)
+    }
+
+
+}
